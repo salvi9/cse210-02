d13251b84feb8a4d953ad81582c95e2d6f9a54fb
antmorera <ant.morera.lb@gmail.com>
2022-10-07 21:45:33 -0600

    Added a main function with the corresponding variables that will run the game

--- __main__.py
+++ __main__.py
@@ -1,4 +1,14 @@
 from game.director import Director
+from game.player import Player
 
-director = Director()
-director.start_game()
+"This function tells the director to start the game at the same time that it *creates* a player "
+def main():
+
+    player = Player()
+    director = Director(player)
+    director.start_game()
+
+
+
+if __name__ == "__main__":
+    main()    
