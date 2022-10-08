2d06394ebfb8e43523213e5c38f8c8510b692f65
Montoya, Alan <gazalem@hotmail.com>
2022-10-07 21:41:42 -0500

    Redesign of game

--- game/director.py
+++ game/director.py
@@ -5,41 +5,60 @@ class Director:
 
     def __init__(self):
         self.card = Card()
-        self.another = ""
-        self.choice = ""
+        self.base_card = self.card.roll_card()
+        self.next_card = self.card.roll_card()
+        self.user_guess = ""
         self.score = 300
         self.keep_playing = True
-        self.points = 0
 
     def start_game(self):
         """Gets things going and will be cycled through every time they choose to play another round"""
         while self.keep_playing:
-            self.get_inputs()
             self.do_updates()
             self.do_outputs()
+            self.get_inputs()
 
     def get_inputs(self):
         """Starts things out by getting assigning card_1"""
-        self.card.card()
+        playAgain = input("Play Again: ")
+        if playAgain.lower() == "y":
+            self.keep_playing = True
+        else:
+            self.keep_playing = False
 
     def do_updates(self):
         """Checks the guess of the player by reaching into Card.py and gives or takes points accordingly."""
-        if self.choice == "low":
-            self.points = self.card.low()
-
-        elif self.choice == "high":
-            self.points = self.card.high()
+        # if self.choice == "low":
+        #     self.points = self.card.low()
+        #
+        # elif self.choice == "high":
+        #     self.points = self.card.high()
+        if self.user_guess.lower() == "high":
+            if self.next_card > self.base_card:
+                self.score += 100
+            else:
+                self.score -= 75
+        else:
+            if self.next_card < self.base_card:
+                self.score += 100
+            else:
+                self.score -= 75
+        self.keep_playing = True if self.score > 0 else False
+        print(f"Your score is: {self.score}")
 
     def do_outputs(self):
         # Outputs the important game information for each round of play. it shows the player
         # their first card and shows them their score. it then asks them if they would like another card
         # if yes, it gets their guess and start everything over.
         # if no, it stops the game.
-        print(f"\nYour new card is: {self.card.card_1}")
-        print(f"Your score is: {self.score}")
-        self.another = input("would you like another card (y/n):")
-        if self.another == "y":
-            self.choice = input("High or Low? [high/low]:")
-            self.do_updates
-        else:
-            self.keep_playing = False
+        # print(f"\nYour new card is: {self.card.card_1}")
+        # print(f"Your score is: {self.score}")
+        # self.another = input("would you like another card (y/n):")
+        # if self.another == "y":
+        #     self.choice = input("High or Low? [high/low]:")
+        #     self.do_updates
+        # else:
+        #     self.keep_playing = False
+        print(f"The card is: {self.base_card}")
+        self.user_guess = input("High or Low? [high/low]: ")
+        print(f"Next Card is: {self.next_card}")
--- game/hilo.py
+++ game/hilo.py
@@ -5,26 +5,56 @@ class Card:
     """card that is chosen and the numbers of turns."""
     
     def __init__(self):
-        self.card_1 = 0
-        self.card_2 = 0
-        self.full_point = 0
+        self.card = 0
 
     "card chooses a random number (1,13)"
-    def card(self):
-        self.card_1 = random.randint(1, 13)
-        return self.card_1        
+    def roll_card(self):
+        self.card = random.randint(1, 13)
+        return self.card
         # self.num_turns += 1
 
-    "If player picks a high number and is correct is worth 100 points. Otherwise they lose 75 points"
-    def high(self):
-        if self.card_2 > self.card_1:
-            return +100
-        elif self.card_2 < self.card_1:
-            return -75
-
-    "If player picks a low number and is correct is worth 100 points. Otherwise they lose 75 points"
-    def low(self):
-        if self.card_2 < self.card_1:
-            return self.full_point + 100
-        elif self.card_2 > self.card_1:
-            return self.full_point - 75
+#     "If player picks a high number and is correct is worth 100 points. Otherwise they lose 75 points"
+#     def high(self):
+#         if self.card_2 > self.card:
+#             return +100
+#         elif self.card_2 < self.card:
+#             return -75
+#
+#     "If player picks a low number and is correct is worth 100 points. Otherwise they lose 75 points"
+#     def low(self):
+#         if self.card_2 < self.card:
+#             return self.full_point + 100
+#         elif self.card_2 > self.card:
+#             return self.full_point - 75
+#
+# def main():
+#     """Entry point of the app"""
+#     score = 300
+#     my_card = Card()
+#     keep_playing = True
+#     base_card = my_card.roll_card()
+#     next_card = my_card.roll_card()
+#     while keep_playing or (score <= 0):
+#         print(f"The card is: {base_card}")
+#         user_guess = input("Higher or lower= [h/l] ")
+#         print(f"Next card was: {next_card}")
+#         if user_guess.lower() == "h":
+#             if next_card > base_card:
+#                 score += 100
+#             else:
+#                 score -= 75
+#         else:
+#             if next_card < base_card:
+#                 score += 100
+#             else:
+#                 score -= 75
+#         print(f"Your score is: {score}")
+#         play_again = input("Play Again? ")
+#         if play_again.lower() == "y":
+#             keep_playing = True
+#         else:
+#             keep_playing = False
+#
+#
+# if __name__ == "__main__":
+#     main()
